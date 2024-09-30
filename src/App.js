fc2d06bbe87c28778b20374c5f5a4b473ad10f8b
DESKTOP-97E2TFH\Patryk Ż 3TP <patryk.zaczek07@gmail.com>
2024-09-30 13:12:52 +0200

    import bootstrap, handle submit, form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.css"; 
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  
+  const [tytul, setTytul] = useState("");
+  const [rodzaj, setRodzaj] = useState("");
+};
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    console.log('Tytul: ${tytul}, Rodzaj: ${rodzaj}');
+
+  return (
+    <h1>Dodaj film</h1>
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor='tytul'>Tytuł filmu:</label>
+    <input
+      type="text"
+      id="tytul"
+      className="form-control"
+      value={tytul}
+      onChange={(e) => setTytul(e.targer.value)}
+      />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="rodzaj">Rodzaj filmu:</label>
+        <select
+        id="rodzaj"
+        className="form-control"
+        value={rodzaj}
+      
+      ></select></div>
+      </form>
+    );
+
+  };
 export default App;
